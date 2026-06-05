c077024df2dd100db7a75ebeb938ff856a204811
mariaaguiar01 <mariaaguiar01@outlook.com>
2026-06-05 14:44:02 +0000

    Formulário 2

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Login</label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <div id="emailHelp" class="form-text">Não compartilhe seu e-mail e senha com ninguém.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Senha</label>
