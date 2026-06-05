9785847fa13d2ccc9f0be692a99eb56ebc17ab9b
mariaaguiar01 <mariaaguiar01@outlook.com>
2026-06-05 17:17:54 +0000

    listinha

--- database/index.php
+++ database/index.php
@@ -1,13 +1,32 @@
 <?php
-
 $conexao = new \PDO('sqlite:database.db');
 $query = $_GET['query'] ?? '';
-$result = $conexao->query($query);
-$resultado = $result->fetchAll(\PDO::FETCH_ASSOC) ?? '';
+$resultado = '';
+try {
+    if (!empty($query)) {
+        $resultados = [];
+        foreach ($conexao->query($query, \PDO::FETCH_ASSOC) as $resultado) {
+            $resultados[] = $resultado;
+        }
+        $resultado = $resultados;
+    }
+} catch (\Throwable $e) {
+    var_dump('erro', $e);
+}
 ?>
 
-<form method="get" action="/">
-    <textarea name="query" id="query"></textarea>
+<form method="get" action="/database">
+    <textarea name="query" id="query" style="width:100%;" rows="10"><?= $query ?></textarea>
     <button type="submit">Executar</button>
 </form>
-<p id="resultado"><?= print_r($resultado) ?></p>
+
+<?php
+echo '<pre>';
+var_dump($resultado);
+?>
+
+<div class="container">
+    <ul>
+        <li>CREATE TABLE itens (id INTEGER PRIMARY KEY, nome VARCHAR(50) NOT NULL, categoria TEXT CHECK (categoria IN ("cozinha","banheiro","lavanderia","area externa")) NOT NULL, quantidade INTEGER NOT NULL, valor DECIMAL(3,2) NOT NULL, checkin BOOLEAN NOT NULL);</li>
+    </ul>
+</div>
